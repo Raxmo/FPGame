8bdfeaa80916f9814ef44cb5f203d06d0ecec910
Raxmo <maxsroe@gmail.com>
2021-10-08 12:54:50 -0600

    [UPDATE 1]
    [COMPLETED]
    - Drawing rutine
    
    [PENDING]
    
    [TODO]

--- FPGame/MainWindow.xaml.cs
+++ FPGame/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace FPGame
 {
@@ -32,32 +33,82 @@ namespace FPGame
 		{
 			InitializeComponent();
 			AllocConsole();
+
+			DT.Tick += new EventHandler(Update);
+			DT.Interval = new TimeSpan(0, 0, 0, 0, 10);
 		}
+
+		public byte[,] buffer;
+		byte[] dbuffer;
+
+		Random rand = new Random();
 
 		public void INITSCREEN()
 		{
 			wbp = new WriteableBitmap(
-				(int)MWin.ActualWidth/10,
-				(int)MWin.ActualHeight/10,
+				(int)MWin.ActualWidth,
+				(int)MWin.ActualHeight,
 				96,
 				96,
-				PixelFormats.BlackWhite,
+				PixelFormats.Gray8,
 				null);
 
 			MAINIMAGE.Source = wbp;
+
+			buffer = new byte[wbp.PixelWidth, wbp.PixelHeight];
+			dbuffer = new byte[buffer.GetLength(0) * buffer.GetLength(1)];
 		}
+
+		public void DrawScreen()
+		{
+			rand.NextBytes(dbuffer);
+
+			int i = 0;
+			for(int y = 0; y < buffer.GetLength(1); y++)
+			{
+				for(int x = 0; x < buffer.GetLength(0); x++)
+				{
+					dbuffer[i] = (byte)(dbuffer[i] < buffer[x, y] ? 255 : 0);
+
+					i++;
+				}
+			}
+
+			Int32Rect rect = new Int32Rect(
+				0,
+				0,
+				buffer.GetLength(0),
+				buffer.GetLength(1));
+
+			wbp.WritePixels(rect, dbuffer, buffer.GetLength(0), 0);
+		}
+
+		DispatcherTimer DT = new DispatcherTimer();
 
 		private void MAINIMAGE_Loaded(object sender, RoutedEventArgs e)
 		{
 			INITSCREEN();
+			DT.Start();
 
-			Test();
+			for(int y = 0; y < buffer.GetLength(1); y++)
+			{
+				for(int x = 0; x < buffer.GetLength(0); x++)
+				{
+					double u = (double)x / buffer.GetLength(0);
+					double v = (double)y / buffer.GetLength(1);
+					double a = (double)buffer.GetLength(1) / buffer.GetLength(0);
+
+					double px = u - 0.5;
+					double py = (v - 0.5) * a;
+
+					buffer[x, y] = (byte)(255 * Math.Sqrt(px * px + py * py));
+				}
+			}
 		}
 
-		public void Test()
+		public void Update(object sender, EventArgs e)
 		{
-			Int32Rect rc = new Int32Rect(0, 0, 1, 1);
-			wbp.WritePixels(rc, new byte[] { 255 }, 1, 10, 10);
+			DrawScreen();
 		}
 	}
 }
